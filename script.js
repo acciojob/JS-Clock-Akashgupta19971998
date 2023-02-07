aa4a7812158873c837f20cc5757c07f22c1d851a
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-02-07 16:14:10 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+let hour=document.getElementsByClassName('hand hour-hand')
+let minute=document.getElementsByClassName('hand min-hand')
+let second=document.getElementsByClassName('hand second-hand')
+
+
+
+function clock(){
+   let date_time = new Date()
+   let hr = date_time.getHours() 
+   let min = date_time.getMinutes()
+   let sec = date_time.getSeconds()
+   hour.innerHTML = hr;
+   min.innerHTML = min;
+   hour.innerHTML = sec;
+}
+
+
+setInterval(clock, 1000)
